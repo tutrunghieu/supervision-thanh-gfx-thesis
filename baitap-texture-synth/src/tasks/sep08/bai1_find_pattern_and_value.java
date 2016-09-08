0dd390a02f31d627e1887cf68e747b7d4e90a19f
tutrunghieu <tutrunghieu@gmail.com>
2016-09-08 19:43:50 +0700

    Add ref

--- baitap-texture-synth/src/tasks/sep08/bai1_find_pattern_and_value.java
+++ baitap-texture-synth/src/tasks/sep08/bai1_find_pattern_and_value.java
@@ -2,6 +2,7 @@ package tasks.sep08;
 
 public class bai1_find_pattern_and_value {
 
+	//xem https://docs.google.com/document/d/1otLpUrJWBvI1oFzFP_OcT2d333EZ8OWH53c_R_7A9h0/edit#
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
